0f7feed0e9322dbc294af55fbc9158252f8c4521
Motter030 <ting921024tim@gmail.com>
2025-06-19 21:08:46 +0800

    Add files via upload

---
+++ questions/嗓音與吞嚥障礙/嗓音與吞嚥障礙110.js
@@ -0,0 +1,752 @@
+var questions_vsd110 = [
+  {
+    "question": "1 有關發聲器官，下列敘述何者錯誤？",
+    "image": "",
+    "year": 110,
+    "options": {
+      "A": "假聲帶發聲（ventricular phonation）通常是低音調（low-pitched）、粗糙（rough）",
+      "B": "舌骨（hyoid bone）沒有和其他的骨頭形成關節",
+      "C": "杓狀軟骨（arytenoid cartilage）主要有 2 個突起，聲突（vocal process）連接聲帶，肌突（muscular process）連接環杓肌（cricoarytenoid muscles）",
+      "D": "甲杓肌（thyroarytenoid muscle）收縮，可降低聲帶振動之基頻（fundamental frequency）以及音量（loudness）"
+    },
+    "answer": [
+      "D"
+    ],
+    "type": "single"
+  },
+  {
+    "question": "2 關於發聲閾壓（phonation threshold pressure），下列敘述何者正確？",
+    "image": "",
+    "year": 110,
+    "options": {
+      "A": "發聲閾壓和聲門下壓無直接關係",
+      "B": "聲帶麻痺患者有時不能發聲，因為發聲閾壓過低",
+      "C": "聲門成聚合型形狀（convergent shape glottis）會產生比較高的發聲閾壓",
+      "D": "發聲閾壓和發聲時所費的力氣無直接關係"
+    },
+    "answer": [
+      "C"
+    ],
+    "type": "single"
+  },
+  {
+    "question": "3 下列那一對腦神經受損後最不會影響嗓音的產生和共振？",
+    "image": "",
+    "year": 110,
+    "options": {
+      "A": "第 7 對腦神經",
+      "B": "第 8 對腦神經",
+      "C": "第 10 對腦神經",
+      "D": "第 12 對腦神經"
+    },
+    "answer": [
+      "B"
+    ],
+    "type": "single"
+  },
+  {
+    "question": "4 有關全身性疾病引起的嗓音異常，下列敘述何者正確？",
+    "image": "",
+    "year": 110,
+    "options": {
+      "A": "感冒時常使用的抗組織胺藥物會造成喉黏膜乾燥而影響嗓音",
+      "B": "胃食道逆流引起的嗓音障礙屬於內分泌問題引起的嗓音異常",
+      "C": "氣喘患者的吸入性類固醇噴劑不會對嗓音有影響",
+      "D": "多發性硬化症的嗓音障礙屬於內分泌問題引起的嗓音異常"
+    },
+    "answer": [
+      "A"
+    ],
+    "type": "single"
+  },
+  {
+    "question": "5 下列那一種疾病的病因有可能是醫源性（iatrogenic），也有可能是原發性（idiopathic）？",
+    "image": "",
+    "year": 110,
+    "options": {
+      "A": "聲帶水腫",
+      "B": "單側聲帶麻痺",
+      "C": "聲帶乳突瘤",
+      "D": "環杓關節硬化"
+    },
+    "answer": [
+      "B"
+    ],
+    "type": "single"
+  },
+  {
+    "question": "6 咽喉逆流（LPR）所造成的嗓音障礙屬於下列何種病理分類？",
+    "image": "",
+    "year": 110,
+    "options": {
+      "A": "聲帶結構改變",
+      "B": "全身性疾病（systemic disease）",
+      "C": "心因性嗓音障礙",
+      "D": "原因不明的嗓音障礙"
+    },
+    "answer": [
+      "B"
+    ],
+    "type": "single"
+  },
+  {
+    "question": "7 下列何者最不常發生在閉合型痙攣性嗓音障礙（adductor spasmodic dysphonia）患者？",
+    "image": "",
+    "year": 110,
+    "options": {
+      "A": "聲門閉合不全",
+      "B": "顫抖（tremor）",
+      "C": "發聲非常困難",
+      "D": "說話容易疲勞"
+    },
+    "answer": [
+      "A"
+    ],
+    "type": "single"
+  },
+  {
+    "question": "8 關於喉部及聲帶疾病，下列敘述何者正確？",
+    "image": "",
+    "year": 110,
+    "options": {
+      "A": "聲帶白斑（leukoplakia）常被認為是一種癌症前期的變化現象，因此不需嗓音治療介入",
+      "B": "聲帶白斑的聽知覺評估結果，和其他原因造成的嗓音異常聽知覺評估結果很容易區分",
+      "C": "持續性嗓音沙啞，或是接受嗓音治療效果不佳且繼續惡化時，應建議做聲帶內視鏡影像檢查",
+      "D": "嗓音沙啞是惡性聲帶腫瘤到末期才出現的症狀"
+    },
+    "answer": [
+      "C"
+    ],
+    "type": "single"
+  },
+  {
+    "question": "9 下列何者不是單側聲帶麻痺患者常見的嗓音症狀？",
+    "image": "",
+    "year": 110,
+    "options": {
+      "A": "氣息聲",
+      "B": "粗糙聲",
+      "C": "說話聲音太大",
+      "D": "說話容易疲勞"
+    },
+    "answer": [
+      "C"
+    ],
+    "type": "single"
+  },
+  {
+    "question": "10 下列何者屬於中樞神經疾病所造成的嗓音異常？①喉上神經麻痺 ②巴金森氏症 ③脊髓側索硬化症（ALS）④重症肌無力（myasthenia gravis）",
+    "image": "",
+    "year": 110,
+    "options": {
+      "A": "①③",
+      "B": "③④",
+      "C": "②③",
+      "D": "②④"
+    },
+    "answer": [
+      "C"
+    ],
+    "type": "single"
+  },
+  {
+    "question": "11 由神經性因素引起的嗓音異常中，可經由症狀來推測損傷的神經功能，下列敘述何者正確？",
+    "image": "",
+    "year": 110,
+    "options": {
+      "A": "音調高低改變功能異常可能是損傷到喉返神經內枝",
+      "B": "音調高低改變功能異常可能是損傷到喉上神經內枝",
+      "C": "音調高低改變功能異常可能是損傷到喉返神經外枝",
+      "D": "音調高低改變功能異常可能是損傷到喉上神經外枝"
+    },
+    "answer": [
+      "D"
+    ],
+    "type": "single"
+  },
+  {
+    "question": "12 關於發聲－呼吸效率分析的測量，下列何者錯誤？",
+    "image": "",
+    "year": 110,
+    "options": {
+      "A": "s/z ratio 是透過發 s 和 z 的時長，測量喉部氣流間接指數",
+      "B": "Boone（1971）提出，正常聲帶的 s/z ratio 接近 1",
+      "C": "Maximum phonation time（MPT）是測量發母音的最長時間，測量時應請患者深吸氣再測量，並以一次為限",
+      "D": "測量 maximum phonation time（MPT）時，治療師應該有標準一致的指導語"
+    },
+    "answer": [
+      "C"
+    ],
+    "type": "single"
+  },
+  {
+    "question": "13 進行喉內視鏡檢查時，須注意檢查程序，才能獲得有效的影像。下列敘述何者正確？",
+    "image": "",
+    "year": 110,
+    "options": {
+      "A": "以硬式喉鏡進行檢查，鏡頭的角度一般是 70 度或 90 度",
+      "B": "檢查時要求受試者發任一長母音至少 5 秒，即可觀察聲帶振動情形",
+      "C": "檢查有兩種形式，軟式及硬式喉鏡，使用軟式喉鏡可以測得個案說話時的發聲情形，且影像的亮度也較清晰",
+      "D": "喉內視鏡屬於客觀儀器檢查，在判讀結果方面有良好判讀者間信度"
+    },
+    "answer": [
+      "A"
+    ],
+    "type": "single"
+  },
+  {
+    "question": "14 關於嗓音聽知覺評估量表 GRBAS、CAPE-V，下列敘述何者正確？",
+    "image": "",
+    "year": 110,
+    "options": {
+      "A": "GRBAS 為一個六點的量表（6-point scale）",
+      "B": "GRBAS 量表包括音高、音強的評估",
+      "C": "CAPE-V 為一個十點的量表（10-point scale）",
+      "D": "CAPE-V 的嗓音樣本包括六個標準句子"
+    },
+    "answer": [
+      "D"
+    ],
+    "type": "single"
+  },
+  {
+    "question": "15 最長發聲時長（maximum phonation time, MPT）是臨床上常使用的評估方式，關於其實施方式及臨床意義，下列敘述何者正確？",
+    "image": "",
+    "year": 110,
+    "options": {
+      "A": "評估時，不限定音高，時間越長越好",
+      "B": "屬於氣體動力學的評估，是反應呼吸功能及肺活量大小的重要指標",
+      "C": "經過嗓音治療，MPT 的增加主要是反應肺容量的增加",
+      "D": "MPT 會受到聲帶振動效率的影響"
+    },
+    "answer": [
+      "D"
+    ],
+    "type": "single"
+  },
+  {
+    "question": "16 關於氣體動力學的評估，下列敘述何者錯誤？",
+    "image": "",
+    "year": 110,
+    "options": {
+      "A": "正常嗓音者的平均氣流速率（average flow rate）大約介於 80～200 cc/second",
+      "B": "聲門下壓的測量是侵入性的檢查程序",
+      "C": "發聲閾值壓力（phonation threshold pressure）是指以一般音量發聲時，可使聲帶產生振動的壓力",
+      "D": "聲門下壓的測量結果可以反應聲門閉合的情形"
+    },
+    "answer": [
+      "C"
+    ],
+    "type": "single"
+  },
+  {
+    "question": "17 關於喉部頻閃攝影觀測與判讀，下列敘述何者錯誤？",
+    "image": "",
+    "year": 110,
+    "options": {
+      "A": "聲帶黏膜波動對稱性（mucosal wave symmetry）是指一個完整的聲帶震動週期，聲門開啟期及閉合期的時間是否一致",
+      "B": "聲帶黏膜波動是指聲帶包膜（cover）在側邊、縱向及垂直向波形的移動動作",
+      "C": "聲帶黏膜波動振幅（amplitude）是指聲帶黏膜往兩側偏移的幅度",
+      "D": "聲帶黏膜波動週期性（periodicity）是指聲帶連續的開啟閉合，在時間上的規律性"
+    },
+    "answer": [
+      "A"
+    ],
+    "type": "single"
+  },
+  {
+    "question": "18 關於聲譜圖（sound spectrogram）的臨床應用，下列敘述何者錯誤？",
+    "image": "",
+    "year": 110,
+    "options": {
+      "A": "不僅能表現聲門音的情形，也能表現聲道變化的特性",
+      "B": "頻率呈現在縱軸，音量則以顏色的深淺來呈現",
+      "C": "與能譜（power spectrum）相同，均可呈現音量、頻率及時間特性",
+      "D": "有寬頻及窄頻兩種現式，均可提供說話之訊噪比（harmonics-to-noise ratio）視覺訊息"
+    },
+    "answer": [
+      "C"
+    ],
+    "type": "single"
+  },
+  {
+    "question": "19 關於手指按壓法（digital manipulation）的嗓音治療策略，下列敘述何者正確？",
+    "image": "",
+    "year": 110,
+    "options": {
+      "A": "對於音高過高，喉部肌肉緊張者，按壓的位置主要在舌骨上肌",
+      "B": "對於單側聲帶麻痺患者，可透過按壓喉部改善聲帶閉合，但只限於按壓患側",
+      "C": "可監控發聲時喉部高度，避免產生不當張力，按壓位置在甲狀軟骨處",
+      "D": "可有效改善聲門閉鎖不全者之發聲生理，屬於生理性嗓音治療（physiologic voice therapy）"
+    },
+    "answer": [
+      "C"
+    ],
+    "type": "single"
+  },
+  {
+    "question": "20 下列何者屬於聲帶功能運動（vocal function exercises）的治療步驟？",
+    "image": "",
+    "year": 110,
+    "options": {
+      "A": "從事發聲運動的過程中，為了避免聲帶受傷，建議使用略帶氣息的聲音",
+      "B": "共有四項發聲運動，第二項運動是使用滑音練習，從最低音調滑至最高音調",
+      "C": "該治療計畫的設計是為期 8～10 週的規律練習後，即可結案",
+      "D": "強調量化個案的進步情形，建議個案將每天的表現與前一天進行比較，以激勵個案"
+    },
+    "answer": [
+      "B"
+    ],
+    "type": "single"
+  },
+  {
+    "question": "21 李先生，50 歲，公司老闆，一個月前接受甲狀腺手術後突然出現嗓音嚴重氣息音、沙啞症狀。喉內視鏡檢查發現左側聲帶麻痺，李先生在休息一個月後，嗓音狀況未見起色。因工作上常需要開會及大量用聲，為了短時間內恢復聲音回到平常工作，下列何種治療方式最合理？",
+    "image": "",
+    "year": 110,
+    "options": {
+      "A": "嗓音治療",
+      "B": "聲帶玻尿酸注射",
+      "C": "第一型甲狀軟骨成型術（Thyroplasty type I）",
+      "D": "禁聲及觀察"
+    },
+    "answer": [
+      "B"
+    ],
+    "type": "single"
+  },
+  {
+    "question": "22 關於全喉切除／喉全切除手術後的言語復健，下列敘述何者錯誤？",
+    "image": "",
+    "year": 110,
+    "options": {
+      "A": "氣管食道發聲瓣（tracheoesophageal prosthesis, TEP），器材使用需定期更換",
+      "B": "氣動式人工助講器與食道語發聲的動力源相同",
+      "C": "食道語通常頻率比較低沉，比較費力，也需較長時間的學習",
+      "D": "電動式助講器建議說話時誇張構音口型，以增加其語音清晰度"
+    },
+    "answer": [
+      "B"
+    ],
+    "type": "single"
+  },
+  {
+    "question": "23 關於嗓音重音治療法（accent method），下列敘述何者正確？",
+    "image": "",
+    "year": 110,
+    "options": {
+      "A": "為防止重音訓練時造成聲帶的負擔過重，練習時的節律要使用相當樂曲的行板（andante）速度即可",
+      "B": "重音治療法是在訓練共鳴系統，可以經由增加共鳴效果來改善嗓音及構音的功能",
+      "C": "重音治療法只使用雙拍節律（two-beat rhythm）進行練習",
+      "D": "重音治療法的理論基礎，是根源於發聲的肌彈力氣體動力理論（myoelastic-aerodynamic theory）進行治療"
+    },
+    "answer": [
+      "D"
+    ],
+    "type": "single"
+  },
+  {
+    "question": "24 學習食道語的重點，在於將空氣導引入食道上方，並適時發聲，下列那一種引入空氣的方式最有效率？",
+    "image": "",
+    "year": 110,
+    "options": {
+      "A": "吞咽注入法（glossopharyngeal injection）",
+      "B": "子音注入法（consonant injection）",
+      "C": "吸引法（inhalation method）",
+      "D": "吞嚥法（swallow method）"
+    },
+    "answer": [
+      "C"
+    ],
+    "type": "single"
+  },
+  {
+    "question": "25 關於 confidential voice therapy，下列敘述何者正確？",
+    "image": "",
+    "year": 110,
+    "options": {
+      "A": "適用的範圍很廣，但不包括聲帶手術後及長期肌肉緊張的患者",
+      "B": "研究證實，該治療法是有效的治療方式，一般以單獨施行此方法為主",
+      "C": "發聲時，聲帶振動的振幅小，可以有效避免聲帶用力撞擊",
+      "D": "是一種輕鬆的發聲方式，音量雖不大，但因為共鳴佳，所以足以適用一般的說話情境"
+    },
+    "answer": [
+      "C"
+    ],
+    "type": "single"
+  },
+  {
+    "question": "26 如圖，懸吊舌骨的軟組織中，(1) 包括下列那一條肌肉？",
+    "image": "images/嗓音與吞嚥障礙/110q26.jpg",
+    "year": 110,
+    "options": {
+      "A": "頦舌骨肌（geniohyoid muscle）",
+      "B": "莖突舌骨肌（stylohyoid muscle）",
+      "C": "二腹肌後腹（posterior belly of digastric muscle）",
+      "D": "甲狀舌骨肌（thyrohoid muscle）"
+    },
+    "answer": [
+      "A"
+    ],
+    "type": "single"
+  },
+  {
+    "question": "27 在口腔期，下顎動作的運動神經元位於腦幹的那一個神經核？",
+    "image": "",
+    "year": 110,
+    "options": {
+      "A": "疑核（nucleus ambiguous）",
+      "B": "孤束核（nucleus tractus solitarus）",
+      "C": "下橄欖核（inferior olivaris nucleus）",
+      "D": "副橄欖核（accessory olivaris nucleus）"
+    },
+    "answer": [
+      "A"
+    ],
+    "type": "single"
+  },
+  {
+    "question": "28 在正常吞嚥過程之咽部延遲時間（pharyngeal delay time）大約多長？",
+    "image": "",
+    "year": 110,
+    "options": {
+      "A": "0.2 秒",
+      "B": "0.8 秒",
+      "C": "1.0 秒",
+      "D": "1.5 秒"
+    },
+    "answer": [
+      "A"
+    ],
+    "type": "single"
+  },
+  {
+    "question": "29 下列那個異常最可能會造成吞嚥前誤吸（aspiration before swallowing）？",
+    "image": "",
+    "year": 110,
+    "options": {
+      "A": "喉部閉合不足",
+      "B": "舌根動作不足",
+      "C": "氣管食道瘻管（tracheoesophageal fistula）",
+      "D": "咽部吞嚥遲緩"
+    },
+    "answer": [
+      "D"
+    ],
+    "type": "single"
+  },
+  {
+    "question": "30 氣管食道瘻管會產生下列何種吞嚥問題？",
+    "image": "",
+    "year": 110,
+    "options": {
+      "A": "誤吸（aspiration）",
+      "B": "滲入（penetration）",
+      "C": "吐出（expectoration）",
+      "D": "殘留（residue）"
+    },
+    "answer": [
+      "A"
+    ],
+    "type": "single"
+  },
+  {
+    "question": "31 下列那個資訊的取得，使用內視鏡吞嚥檢查（FEES）比螢光吞嚥攝影（MBS）更清楚？",
+    "image": "",
+    "year": 110,
+    "options": {
+      "A": "分泌物累積（pooled secretion）",
+      "B": "吞嚥中有無吸入現象（aspiration during swallow）",
+      "C": "舌部前後的運動",
+      "D": "環咽肌的開合"
+    },
+    "answer": [
+      "A"
+    ],
+    "type": "single"
+  },
+  {
+    "question": "32 下列何者無法從電視螢光吞嚥攝影檢查得知結果？",
+    "image": "",
+    "year": 110,
+    "options": {
+      "A": "咽部隱窩殘留物的量",
+      "B": "咽部期吞嚥遲滯時間",
+      "C": "吞嚥前出現吸入現象",
+      "D": "下食道括約肌打開程度"
+    },
+    "answer": [
+      "D"
+    ],
+    "type": "single"
+  },
+  {
+    "question": "33 如果患者害怕吞嚥，在螢光吞嚥攝影檢查時會看到什麼現象？",
+    "image": "",
+    "year": 110,
+    "options": {
+      "A": "食物殘留在口腔底部",
+      "B": "舌部有搜尋動作",
+      "C": "舌部不接觸硬顎",
+      "D": "分次吞嚥"
+    },
+    "answer": [
+      "D"
+    ],
+    "type": "single"
+  },
+  {
+    "question": "34 下列那一項檢查能夠提供最佳的咽部上方解剖圖像資訊？",
+    "image": "",
+    "year": 110,
+    "options": {
+      "A": "電視內視鏡檢查（videoendoscopy）",
+      "B": "電視螢光攝影檢查（videofluoroscopy）",
+      "C": "電影螢光透視檢查（cinefluorography）",
+      "D": "閃爍造影術（scintigraphy）"
+    },
+    "answer": [
+      "A"
+    ],
+    "type": "single"
+  },
+  {
+    "question": "35 關於吞嚥障礙篩檢程序（screening），下列敘述何者正確？",
+    "image": "",
+    "year": 110,
+    "options": {
+      "A": "篩檢程序是找出口咽部解剖構造與生理機能的吞嚥問題",
+      "B": "篩檢程序是一種費時、安全、成本效益低的方法",
+      "C": "篩檢程序可以測量舌根後移的程度",
+      "D": "篩檢程序可以觀察到吞嚥效率不佳、嗆咳等症狀"
+    },
+    "answer": [
+      "D"
+    ],
+    "type": "single"
+  },
+  {
+    "question": "36 電視螢光吞嚥攝影檢查時，看到咽部吞嚥已啟動，但食團仍殘留會厭谿，最可能的異常為下列何者？",
+    "image": "",
+    "year": 110,
+    "options": {
+      "A": "咽部吞嚥啟動延遲",
+      "B": "環咽肌張力過高",
+      "C": "舌根收縮不足",
+      "D": "後咽壁收縮不足"
+    },
+    "answer": [
+      "C"
+    ],
+    "type": "single"
+  },
+  {
+    "question": "37 改良鋇劑吞嚥檢查（modified barium swallow）時的正面像，主要是要觀察到下列那些情況？①兩側梨狀窩殘留情形 ②咽部期通過時間 ③會厭軟骨往後傾倒的幅度 ④會厭谿單側或雙側殘留",
+    "image": "",
+    "year": 110,
+    "options": {
+      "A": "②③",
+      "B": "①④",
+      "C": "①②",
+      "D": "③④"
+    },
+    "answer": [
+      "B"
+    ],
+    "type": "single"
+  },
+  {
+    "question": "38 對於吞嚥反射延遲的患者，最不建議使用下列那一種方法幫助吞嚥？",
+    "image": "",
+    "year": 110,
+    "options": {
+      "A": "低頭姿勢",
+      "B": "舌把持策略（tongue holding maneuver）",
+      "C": "吸吮吞嚥（suck swallow）",
+      "D": "溫度觸覺刺激"
+    },
+    "answer": [
+      "B"
+    ],
+    "type": "single"
+  },
+  {
+    "question": "39 患者有單側左邊咽部麻痺，下列何種姿勢進行吞嚥最適當？",
+    "image": "",
+    "year": 110,
+    "options": {
+      "A": "頭傾向左側",
+      "B": "頭向前低",
+      "C": "頭轉向左側",
+      "D": "頭轉向右側"
+    },
+    "answer": [
+      "C"
+    ],
+    "type": "single"
+  },
+  {
+    "question": "40 舌部功能異常的患者，可以學習下列何種方法以補充大量的液體？",
+    "image": "",
+    "year": 110,
+    "options": {
+      "A": "傾卸吞嚥法（dump and swallow）",
+      "B": "上聲門吞嚥法（supraglottic swallow）",
+      "C": "Masako 手法（Masako maneuver）",
+      "D": "吸吮吞嚥法（suck swallow）"
+    },
+    "answer": [
+      "A"
+    ],
+    "type": "single"
+  },
+  {
+    "question": "41 下列那些吞嚥手法可以改善舌根部的動作？①上聲門吞嚥法 ②超上聲門吞嚥法 ③用力吞嚥法 ④孟德森吞嚥法 ⑤Masako maneuver",
+    "image": "",
+    "year": 110,
+    "options": {
+      "A": "①③",
+      "B": "②④",
+      "C": "④⑤",
+      "D": "②③"
+    },
+    "answer": [
+      "D"
+    ],
+    "type": "single"
+  },
+  {
+    "question": "42 神經性吞嚥障礙患者於螢光吞嚥攝影檢查中出現「喉部向前與向上拉提不足」時，同時出現的問題並不包含下列何者？",
+    "image": "",
+    "year": 110,
+    "options": {
+      "A": "吞嚥前食團滲入或吸入呼吸道",
+      "B": "喉部閉合不完全",
+      "C": "環咽肌（cricopharyngeal muscle）開啟不足",
+      "D": "吞嚥後梨狀窩（pyriform sinus）仍有食團殘留"
+    },
+    "answer": [
+      "A"
+    ],
+    "type": "single"
+  },
+  {
+    "question": "43 有關吞嚥失用症（swallowing apraxia）的特徵，下列敘述何者正確？",
+    "image": "",
+    "year": 110,
+    "options": {
+      "A": "好發於右大腦前區中風",
+      "B": "多給予口頭提示有助於吞嚥動作的進行",
+      "C": "通常咽部期本身的動作是正常的",
+      "D": "增加口腔知覺的策略對這類患者沒有幫助"
+    },
+    "answer": [
+      "C"
+    ],
+    "type": "single"
+  },
+  {
+    "question": "44 有關巴金森氏症患者的吞嚥異常，下列敘述何者錯誤？",
+    "image": "",
+    "year": 110,
+    "options": {
+      "A": "舌頭重複的搖滾動作（rocking rolling action）是其特有的動作",
+      "B": "主要影響口腔準備期與口腔期的動作，咽部期的吞嚥動作較不受影響",
+      "C": "出現靜止型震顫的部位，包含頭頸部、下顎（mandible）與舌頭",
+      "D": "藥物治療的效果，會影響吞嚥異常的表現"
+    },
+    "answer": [
+      "B"
+    ],
+    "type": "single"
+  },
+  {
+    "question": "45 一位 91 歲的爺爺被帶來看診，其女兒主訴「吃飯時我爸爸經常會拿湯匙柄去撮弄菜餚」。請問這位爺爺最可能出現什麼問題？",
+    "image": "",
+    "year": 110,
+    "options": {
+      "A": "渥尼克失語症（Wernicke's aphasia）",
+      "B": "進食失用症（feeding apraxia）",
+      "C": "吞嚥失用症（swallowing apraxia）",
+      "D": "聯想障礙性失用症（ideational apraxia）"
+    },
+    "answer": [
+      "B"
+    ],
+    "type": "single"
+  },
+  {
+    "question": "46 有關頭頸癌 TNM 的腫瘤分級系統，下列敘述何者錯誤？",
+    "image": "",
+    "year": 110,
+    "options": {
+      "A": "T：代表腫瘤的數量",
+      "B": "N：代表頸部淋巴結的轉移",
+      "C": "M：代表遠端的轉移",
+      "D": "TNM 系統的結果和腫瘤的分期有關"
+    },
+    "answer": [
+      "A"
+    ],
+    "type": "single"
+  },
+  {
+    "question": "47 關於頭頸癌患者接受放射線治療後的副作用，下列何者不是造成吞嚥障礙的主要原因？",
+    "image": "",
+    "year": 110,
+    "options": {
+      "A": "咽喉水腫",
+      "B": "唾液分泌減少",
+      "C": "甲狀腺功能低下症",
+      "D": "周邊神經病變"
+    },
+    "answer": [
+      "C"
+    ],
+    "type": "single"
+  },
+  {
+    "question": "48 有關口腔癌手術方式造成的吞嚥異常，下列敘述何者錯誤？",
+    "image": "",
+    "year": 110,
+    "options": {
+      "A": "舌切除超過 50%時，會造成舌與硬顎接觸不良，控制食團的能力降低",
+      "B": "前口底切除的患者，若口底肌群也有切除，會影響食團咽部通過時間",
+      "C": "切除前咽門弓，可能造成咽部期吞嚥啟動困難或延遲",
+      "D": "前口底切除時，用舌與嘴唇做縫合的方式，比舌沒有縫合的患者，有更正常的口腔通過時間"
+    },
+    "answer": [
+      "D"
+    ],
+    "type": "single"
+  },
+  {
+    "question": "49 對於頭頸癌合併化學放射治療的病患，為了提升其吞嚥功能，何時是進行吞嚥訓練的最佳時機？",
+    "image": "",
+    "year": 110,
+    "options": {
+      "A": "治療前開始訓練",
+      "B": "治療中開始訓練",
+      "C": "治療後立刻開始訓練",
+      "D": "治療後 6 週開始訓練"
+    },
+    "answer": [
+      "A"
+    ],
+    "type": "single"
+  },
+  {
+    "question": "50 關於嬰兒反射性與主動性餵食（feeding）階段的差別，下列敘述何者正確？",
+    "image": "",
+    "year": 110,
+    "options": {
+      "A": "反射性餵食階段的口腔期是有意志的",
+      "B": "主動性餵食階段可包含攝食液體與固體等各種質地的食物",
+      "C": "啟動反射性吸吮的中樞在大腦皮質",
+      "D": "主動性餵食階段時舌部的動作是單向性的活動"
+    },
+    "answer": [
+      "B"
+    ],
+    "type": "single"
+  }
+];
